a948951966c1aa0b67ac70f55df1e57cd1234336
etouraille <eouard.touraille@gmail.com>
2022-11-28 17:18:37 +0100

    co

--- manager/src/app/logged/income/income.component.ts
+++ manager/src/app/logged/income/income.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {SubscribeComponent} from "../../../lib/component/subscribe/subscribe.component";
 import {HttpClient} from "@angular/common/http";
 import {switchMap} from "rxjs";
+import {DomSanitizer} from "@angular/platform-browser";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-income',
@@ -12,9 +14,13 @@ export class IncomeComponent extends SubscribeComponent implements OnInit {
 
   user: any = null;
   expenses: any[] = [];
+  bill: any = null;
+  url: any = null;
 
   constructor(
     private http: HttpClient,
+    private sanitizer: DomSanitizer,
+    private toastR: ToastrService,
   ) {
     super();
   }
@@ -28,6 +34,12 @@ export class IncomeComponent extends SubscribeComponent implements OnInit {
       return this.http.get('api/expenses?userId=' + user.id);
     })).subscribe((data: any) => {
       this.expenses = data['hydra:member'];
+      this.bill = this.expenses[0]?.incomeData?.file;
+      if(this.bill) {
+        setTimeout(() => {
+          this.url =  this.sanitizer.bypassSecurityTrustResourceUrl( 'https://drive.google.com/viewerng/viewer?embedded=true&url=' +this.bill +'#toolbar=0&scrollbar=0') ;
+        });
+      }
     }))
   }
 
@@ -35,16 +47,20 @@ export class IncomeComponent extends SubscribeComponent implements OnInit {
     this.add(
       this
         .http
-        .get('api/expenses/process?userId=' + this.user.id)
+        .get('api/expense/process?userId=' + this.user.id)
         .subscribe((data: any) => {
-          //TODO popin message
-          console.log(data);
+          if(data['hydra:member'][0].success) {
+            this.toastR.success('Paiement réussi !');
+          } else if(data['hydra:member'][0].error) {
+            this.toastR.error(data['hydra:member'][0].error);
+          }
+          this.changeUserId(this.user.id);
         }));
   }
 
   generate() {
     this.add(this.http.get('api/export/income?userId=' + this.user.id).subscribe((data: any) => {
-      console.log(data);
+      this.changeUserId(this.user.id);
     }))
   }
 }
