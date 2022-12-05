9a4e09a9f6fa775092946a805601b3bb8da3a4d9
etouraille <eouard.touraille@gmail.com>
2022-12-05 11:47:17 +0100

    [#41] Fix build

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://queel.io/',
+  cdn: 'https://cdn.queel.io/',
+  stripe: 'pk_test_51M5VUJE2cCuT5clEeeLgqsVbClfFzBuLWQbGNWDRekYxKxFRG2mDmpitmvnBayHi9GWNNSBxghxwPC9HaeK6fkgE00VEh9lRDu',
+  local: 'http://app.queel.io/',
+  app: 'app',
 };
