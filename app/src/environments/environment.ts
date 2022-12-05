ae721e8daa30b3f73284d58ab3be6cd87d5ab757
etouraille <eouard.touraille@gmail.com>
2022-12-05 10:10:08 +0100

    [#33] Warning message if reservation date is in the past

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'https://queel.io/',
-  cdn: 'https://cdn.queel.io/',
+  api: 'http://localhost:8080/',
+  cdn: 'http://localhost:3000/',
   stripe: 'pk_test_51M5VUJE2cCuT5clEeeLgqsVbClfFzBuLWQbGNWDRekYxKxFRG2mDmpitmvnBayHi9GWNNSBxghxwPC9HaeK6fkgE00VEh9lRDu',
   local: 'http://localhost:4200/',
   app: 'app',
