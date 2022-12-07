787e581d9e786878f69c14c78ef81fab2feaa2e0
etouraille <eouard.touraille@gmail.com>
2022-12-07 09:08:28 +0100

    [#31] change google key

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   stripe: 'pk_test_51M5VUJE2cCuT5clEeeLgqsVbClfFzBuLWQbGNWDRekYxKxFRG2mDmpitmvnBayHi9GWNNSBxghxwPC9HaeK6fkgE00VEh9lRDu',
   local: 'http://app.queel.io/',
   app: 'app',
-  google: '1075289619931-nnknnpqbmk3gfo66mmobe4dk30bae6l6.apps.googleusercontent.com'
+  google: '687129491368-m6mmaopj2b221vsql4r03mduhja677o1.apps.googleusercontent.com'
 };
